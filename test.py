58614a6de10640246d5e933d93e341739077aa97
James Byron <jamesbyron84@gmail.com>
2018-06-06 00:17:36 -0700

    work

--- test.py
+++ test.py
@@ -13,3 +13,8 @@ if (len(sys.argv) > 1):
 		fileList.append(line.strip().split())
 print(fileList)
 {"name": "name", "hash": "hash", "size": 0, "level": 0}
+
+1) Check that rode is up
+2) Set up the one node only
+3) Upload ifles to that node
+4) Run the receive script
